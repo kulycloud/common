a68defee52212b837da8a263ee7f56605939b7ba
themmj <themmj@users.noreply.github.com>
2020-11-09 22:07:55 +0100

    KU-39 KU-55 Generalize grpc stream logic

--- http/grpc.go
+++ http/grpc.go
@@ -1,96 +1,42 @@
 package http
 
 import (
-	"errors"
 	protoHttp "github.com/kulycloud/protocol/http"
-	"io"
 )
 
-// client
+func receive(stream grpcStream, object chunkable) error {
+	chunk, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	err = object.fromChunk(chunk)
+	if err != nil {
+		return err
+	}
+	object.getBody().setStream(stream)
+	return nil
+}
 
-func sendRequest(grpcStream protoHttp.Http_ProcessRequestClient, request *HttpRequest) error {
-	var err error
-	reqStream := newStreamFromRequest(request)
+func send(stream grpcStream, object chunkable) error {
+	err := stream.Send(object.toChunk())
+	if err != nil {
+		return err
+	}
+	object.getBody().toStream()
 	for {
-		reqChunk, ok := <-reqStream
-		if ok {
-			err = grpcStream.Send(reqChunk)
-			if err != nil {
-				break
-			}
-		} else {
-			err = grpcStream.CloseSend()
+		chunk, ok := <-object.getBody().sendChannel
+		if !ok {
 			break
 		}
+		err = stream.Send(chunk)
+		if err != nil {
+			return err
+		}
+	}
+	// properly close the send stream if performed by a client
+	clientStream, isClientStream := stream.(protoHttp.Http_ProcessRequestClient)
+	if isClientStream {
+		err = clientStream.CloseSend()
 	}
 	return err
-}
-
-func receiveResponse(grpcStream protoHttp.Http_ProcessRequestClient) (*HttpResponse, error) {
-	var err error
-	var resStream = make(chan *protoHttp.ResponseChunk)
-	go func() {
-		var resChunk *protoHttp.ResponseChunk
-		for {
-			resChunk, err = grpcStream.Recv()
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					err = nil
-					break
-				}
-				break
-			}
-			resStream <- resChunk
-		}
-		close(resStream)
-	}()
-	if err != nil {
-		return nil, err
-	}
-	response := newResponseFromStream(resStream)
-	return response, err
-}
-
-// server
-
-func sendResponse(grpcStream protoHttp.Http_ProcessRequestServer, response *HttpResponse) error {
-	var err error
-	resStream := newStreamFromResponse(response)
-	for {
-		resChunk, ok := <-resStream
-		if ok {
-			err = grpcStream.Send(resChunk)
-			if err != nil {
-				break
-			}
-		} else {
-			break
-		}
-	}
-	return err
-}
-
-func receiveRequest(grpcStream protoHttp.Http_ProcessRequestServer) (*HttpRequest, error) {
-	var err error
-	var reqStream = make(chan *protoHttp.RequestChunk)
-	go func() {
-		var reqChunk *protoHttp.RequestChunk
-		for {
-			reqChunk, err = grpcStream.Recv()
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					err = nil
-					break
-				}
-				break
-			}
-			reqStream <- reqChunk
-		}
-		close(reqStream)
-	}()
-	if err != nil {
-		return nil, err
-	}
-	request := newRequestFromStream(reqStream)
-	return request, err
 }
