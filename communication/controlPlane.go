58877d6be9eca8e1a4270da5ae29e8e93880930b
themmj <themmj@users.noreply.github.com>
2021-03-12 20:51:58 +0100

    KU-104 Add event listening logic and proper synchronization

--- communication/controlPlane.go
+++ communication/controlPlane.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/kulycloud/common/logging"
 	protoCommon "github.com/kulycloud/protocol/common"
@@ -14,20 +15,21 @@ import (
 var logger = logging.GetForComponent("common-communication")
 
 type ControlPlaneCommunicator struct {
-	connection *grpc.ClientConn
+	connection         *grpc.ClientConn
 	controlPlaneClient protoControlPlane.ControlPlaneClient
 
-    storageChangedHandlers []StorageChangedHandler
-    configurationChangedHandlers []ConfigurationChangedHandler
-    clusterChangedHandlers []ClusterChangedHandler
+	storageChangedHandlers       []StorageChangedHandler
+	configurationChangedHandlers []ConfigurationChangedHandler
+	clusterChangedHandlers       []ClusterChangedHandler
+	identifier                   string
 }
 
 func NewControlPlaneCommunicator() *ControlPlaneCommunicator {
 	return &ControlPlaneCommunicator{
-        storageChangedHandlers: make([]StorageChangedHandler, 0),
-        configurationChangedHandlers: make([]ConfigurationChangedHandler, 0),
-        clusterChangedHandlers: make([]ClusterChangedHandler, 0),
-    }
+		storageChangedHandlers:       make([]StorageChangedHandler, 0),
+		configurationChangedHandlers: make([]ConfigurationChangedHandler, 0),
+		clusterChangedHandlers:       make([]ClusterChangedHandler, 0),
+	}
 }
 
 func (communicator *ControlPlaneCommunicator) Connect(host string, port uint32) error {
@@ -39,94 +41,167 @@ func (communicator *ControlPlaneCommunicator) Connect(host string, port uint32) 
 	return nil
 }
 
-func (communicator *ControlPlaneCommunicator) RegisterThisService(ctx context.Context, typeName string, ownHost string, ownPort uint32 ) error {
-	stream, err := communicator.controlPlaneClient.RegisterComponent(ctx, &protoControlPlane.RegisterComponentRequest {
-		Type:     typeName,
-		Endpoint: &protoCommon.Endpoint{
+// Register service asynchronosly
+// the returned channel will either receive nil or an error that was encountered during setup, then close the channel
+func (communicator *ControlPlaneCommunicator) RegisterThisService(ctx context.Context, typeName string, ownHost string, ownPort uint32) <-chan error {
+	done := make(chan error)
+	go func() {
+		endpoint := &protoCommon.Endpoint{
 			Host: ownHost,
 			Port: ownPort,
-		},
-	})
+		}
+		stream, err := communicator.controlPlaneClient.RegisterComponent(ctx, &protoControlPlane.RegisterComponentRequest{
+			Type:     typeName,
+			Endpoint: endpoint,
+		})
 
-	if err != nil {
-		return fmt.Errorf("error from control-plane during connection: %w", err)
-	}
+		if err != nil {
+			done <- err
+			return
+		}
 
-    go func() {
-        for {
-            event, err := stream.Recv()
-            if err != nil {
-                if err != io.EOF {
-                    // might want to log the error
-                }
-                return
-            }
-            communicator.processEvent(event)
-        }
-    }()
+		// validate the stream has been created
+		_, err = stream.Recv()
+		if err != nil {
+			done <- err
+			return
+		}
 
-	return nil
+		// necessary to identify itself to the control plane
+		communicator.identifier = NewIdentifierFromEndpoint(endpoint)
+
+		// start receiving events from stream in the background
+		go func() {
+			for {
+				event, err := stream.Recv()
+				if err != nil {
+					if err != io.EOF {
+						logger.Warnw("could not receive data from stream", "error", err)
+					}
+					return
+				}
+				communicator.processEvent(event)
+			}
+		}()
+		done <- nil
+		close(done)
+
+		// keep this background task running until the stream is closed
+		// otherwise the stream would be garbage collected, in turn closing the context
+		<-stream.Context().Done()
+		logger.Info("event stream closed")
+	}()
+
+	return done
 }
 
 func (communicator *ControlPlaneCommunicator) CreateEvent(event Event) error {
-    _, err := communicator.controlPlaneClient.CreateEvent(context.Background(), event.ToGrpcEvent())
-    return err
+	_, err := communicator.controlPlaneClient.CreateEvent(context.Background(), event.ToGrpcEvent())
+	return err
 }
 
+// add handler to the list of handlers for StorageChanged events and requests control plane to listen on StorageChanged
 func (communicator *ControlPlaneCommunicator) RegisterStorageChangedHandler(handler StorageChangedHandler) error {
-    request := newListenToEventRequest(StorageChangedEvent)
-    _, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
-    if err != nil {
-        return err
-    }
-    communicator.storageChangedHandlers = append(communicator.storageChangedHandlers, handler)
-    return nil
+	request := newListenToEventRequest(communicator.identifier, StorageChangedEvent)
+	_, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
+	if err != nil {
+		return err
+	}
+	communicator.storageChangedHandlers = append(communicator.storageChangedHandlers, handler)
+	return nil
 }
 
+// add handler to the list of handlers for ConfigurationChanged events and requests control plane to listen on ConfigurationChanged
 func (communicator *ControlPlaneCommunicator) RegisterConfigurationChangedHandler(handler ConfigurationChangedHandler) error {
-    request := newListenToEventRequest(ConfigurationChangedEvent)
-    _, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
-    if err != nil {
-        return err
-    }
-    communicator.configurationChangedHandlers = append(communicator.configurationChangedHandlers, handler)
-    return nil
+	request := newListenToEventRequest(communicator.identifier, ConfigurationChangedEvent)
+	_, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
+	if err != nil {
+		return err
+	}
+	communicator.configurationChangedHandlers = append(communicator.configurationChangedHandlers, handler)
+	return nil
 }
 
+// add handler to the list of handlers for ClusterChanged events and requests control plane to listen on ClusterChanged
 func (communicator *ControlPlaneCommunicator) RegisterClusterChangedHandler(handler ClusterChangedHandler) error {
-    request := newListenToEventRequest(ClusterChangedEvent)
-    _, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
-    if err != nil {
-        return err
-    }
-    communicator.clusterChangedHandlers = append(communicator.clusterChangedHandlers, handler)
-    return nil
+	request := newListenToEventRequest(communicator.identifier, ClusterChangedEvent)
+	_, err := communicator.controlPlaneClient.ListenToEvent(context.Background(), request)
+	if err != nil {
+		return err
+	}
+	communicator.clusterChangedHandlers = append(communicator.clusterChangedHandlers, handler)
+	return nil
 }
 
+// Register to the control plane asynchronosly
+// If it cannot register to the control plane within 5 tries it will panic as it does not make sense to continue running
+// If it does not panic the returned channel will receive a valid *ControlPlaneCommunicator
+func RegisterToControlPlane(typeName string, host string, port uint32, cpHost string, cpPort uint32) <-chan *ControlPlaneCommunicator {
+	communicator := make(chan *ControlPlaneCommunicator)
+
+	go func() {
+		var err error
+		var controlPlaneComm *ControlPlaneCommunicator
+		numberOfTries := 0
+		for {
+			numberOfTries++
+			controlPlaneComm, err = register(typeName, host, port, cpHost, cpPort)
+			if err == nil || numberOfTries > 5 {
+				break
+			}
+
+			logger.Info("Retrying in 5s...")
+			time.Sleep(5 * time.Second)
+		}
+		if err != nil {
+			logger.Panicw("could not register to control plane", "lastError", err)
+		}
+
+		communicator <- controlPlaneComm
+		close(communicator)
+	}()
+
+	return communicator
+}
 
 func (communicator *ControlPlaneCommunicator) processEvent(event *protoCommon.Event) {
-    switch EventType(event.Type) {
-        case StorageChangedEvent:
-            sc := (*StorageChanged)(event.GetStorageChanged())
-            if sc != nil {
-                for _, handler := range communicator.storageChangedHandlers {
-                    handler(sc)
-                }
-            }
-        case ConfigurationChangedEvent:
-            cc := (*ConfigurationChanged)(event.GetConfigurationChanged())
-            if cc != nil {
-                for _, handler := range communicator.configurationChangedHandlers {
-                    handler(cc)
-                }
-            }
-        case ClusterChangedEvent:
-            cc := (*ClusterChanged)(event.GetClusterChanged())
-            if cc != nil {
-                for _, handler := range communicator.clusterChangedHandlers {
-                    handler(cc)
-                }
-            }
-    }
+	switch EventType(event.Type) {
+	case StorageChangedEvent:
+		sc := (*StorageChanged)(event.GetStorageChanged())
+		if sc != nil {
+			for _, handler := range communicator.storageChangedHandlers {
+				handler(sc)
+			}
+		}
+	case ConfigurationChangedEvent:
+		cc := (*ConfigurationChanged)(event.GetConfigurationChanged())
+		if cc != nil {
+			for _, handler := range communicator.configurationChangedHandlers {
+				handler(cc)
+			}
+		}
+	case ClusterChangedEvent:
+		cc := (*ClusterChanged)(event.GetClusterChanged())
+		if cc != nil {
+			for _, handler := range communicator.clusterChangedHandlers {
+				handler(cc)
+			}
+		}
+	}
 }
 
+func register(typeName string, host string, port uint32, cpHost string, cpPort uint32) (*ControlPlaneCommunicator, error) {
+	comm := NewControlPlaneCommunicator()
+	err := comm.Connect(cpHost, cpPort)
+	if err != nil {
+		logger.Errorw("Could not connect to control-plane", "error", err)
+		return nil, err
+	}
+	err = <-comm.RegisterThisService(context.Background(), typeName, host, port)
+	if err != nil {
+		logger.Errorw("Could not register service", "error", err)
+		return nil, err
+	}
+	logger.Info("Registered to control-plane")
+	return comm, nil
+}
--- communication/events.go
+++ communication/events.go
@@ -1,101 +1,106 @@
 package communication
 
 import (
-    protoCommon "github.com/kulycloud/protocol/common"
-    protoControlPlane "github.com/kulycloud/protocol/control-plane"
+	"fmt"
+	protoCommon "github.com/kulycloud/protocol/common"
+	protoControlPlane "github.com/kulycloud/protocol/control-plane"
 )
 
 type EventType string
 
 const (
-    StorageChangedEvent EventType = "storageChanged"
-    ConfigurationChangedEvent EventType = "configurationChanged"
-    ClusterChangedEvent EventType = "clusterChanged"
+	StorageChangedEvent       EventType = "storageChanged"
+	ConfigurationChangedEvent EventType = "configurationChanged"
+	ClusterChangedEvent       EventType = "clusterChanged"
 )
 
 type Event interface {
-    GetType() EventType
-    ToGrpcEvent() *protoCommon.Event
+	GetType() EventType
+	ToGrpcEvent() *protoCommon.Event
 }
 
 type StorageChanged protoCommon.StorageChangedEvent
 
 func NewStorageChanged(endpoints []*protoCommon.Endpoint) *StorageChanged {
-    return &StorageChanged{
-        Endpoints: endpoints,
-    }
+	return &StorageChanged{
+		Endpoints: endpoints,
+	}
 }
 
 func (e *StorageChanged) GetType() EventType {
-    return StorageChangedEvent
+	return StorageChangedEvent
 }
 
 func (e *StorageChanged) ToGrpcEvent() *protoCommon.Event {
-    return &protoCommon.Event{
-        Type: string(StorageChangedEvent),
-        Data: &protoCommon.Event_StorageChanged{
-            StorageChanged: (*protoCommon.StorageChangedEvent)(e),
-        },
-    }
+	return &protoCommon.Event{
+		Type: string(StorageChangedEvent),
+		Data: &protoCommon.Event_StorageChanged{
+			StorageChanged: (*protoCommon.StorageChangedEvent)(e),
+		},
+	}
 }
 
 type ConfigurationChanged protoCommon.ConfigurationChangedEvent
 
 func NewConfigurationChanged(namespace string) *ConfigurationChanged {
-    return &ConfigurationChanged{
-        Source: NewResource(namespace),
-    }
+	return &ConfigurationChanged{
+		Source: NewResource(namespace),
+	}
 }
 
 func (e *ConfigurationChanged) GetType() EventType {
-    return ConfigurationChangedEvent
+	return ConfigurationChangedEvent
 }
 
 func (e *ConfigurationChanged) ToGrpcEvent() *protoCommon.Event {
-    return &protoCommon.Event{
-        Type: string(ConfigurationChangedEvent),
-        Data: &protoCommon.Event_ConfigurationChanged{
-            ConfigurationChanged: (*protoCommon.ConfigurationChangedEvent)(e),
-        },
-    }
+	return &protoCommon.Event{
+		Type: string(ConfigurationChangedEvent),
+		Data: &protoCommon.Event_ConfigurationChanged{
+			ConfigurationChanged: (*protoCommon.ConfigurationChangedEvent)(e),
+		},
+	}
 }
 
 type ClusterChanged protoCommon.ClusterChangedEvent
 
 func NewClusterChanged(namespace string) *ClusterChanged {
-    return &ClusterChanged{
-        Source: NewResource(namespace),
-    }
+	return &ClusterChanged{
+		Source: NewResource(namespace),
+	}
 }
 
 func (e *ClusterChanged) GetType() EventType {
-    return ClusterChangedEvent
+	return ClusterChangedEvent
 }
 
 func (e *ClusterChanged) ToGrpcEvent() *protoCommon.Event {
-    return &protoCommon.Event{
-        Type: string(ClusterChangedEvent),
-        Data: &protoCommon.Event_ClusterChanged{
-            ClusterChanged: (*protoCommon.ClusterChangedEvent)(e),
-        },
-    }
+	return &protoCommon.Event{
+		Type: string(ClusterChangedEvent),
+		Data: &protoCommon.Event_ClusterChanged{
+			ClusterChanged: (*protoCommon.ClusterChangedEvent)(e),
+		},
+	}
 }
 
 type (
-    StorageChangedHandler func(*StorageChanged)
-    ConfigurationChangedHandler func(*ConfigurationChanged)
-    ClusterChangedHandler func(*ClusterChanged)
+	StorageChangedHandler       func(*StorageChanged)
+	ConfigurationChangedHandler func(*ConfigurationChanged)
+	ClusterChangedHandler       func(*ClusterChanged)
 )
 
 func NewResource(namespace string) *protoCommon.Resource {
-    return &protoCommon.Resource{
-        Namespace: namespace,
-    }
+	return &protoCommon.Resource{
+		Namespace: namespace,
+	}
 }
 
-func newListenToEventRequest(eventType EventType) *protoControlPlane.ListenToEventRequest {
-    return &protoControlPlane.ListenToEventRequest{
-        Type: string(eventType),
-    }
+func newListenToEventRequest(identifier string, eventType EventType) *protoControlPlane.ListenToEventRequest {
+	return &protoControlPlane.ListenToEventRequest{
+		Type:        string(eventType),
+		Destination: identifier,
+	}
 }
 
+func NewIdentifierFromEndpoint(endpoint *protoCommon.Endpoint) string {
+	return fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port)
+}
