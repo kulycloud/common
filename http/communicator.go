f4eb2a8f6f0f891aa0c3b04ce4c67820f12ae35f
themmj <themmj@users.noreply.github.com>
2021-03-18 14:05:56 +0100

    KU-65 Rework communicator load balancing

--- http/communicator.go
+++ http/communicator.go
@@ -3,6 +3,10 @@ package http
 import (
 	"context"
 	"errors"
+	"sort"
+	"sync"
+	"time"
+
 	"github.com/kulycloud/common/communication"
 	protoCommon "github.com/kulycloud/protocol/common"
 	protoHttp "github.com/kulycloud/protocol/http"
@@ -11,8 +15,15 @@ import (
 var _ communication.RemoteComponent = &Communicator{}
 
 type Communicator struct {
-	communication.ComponentCommunicator
-	httpClient protoHttp.HttpClient
+	componentCommunicator communication.ComponentCommunicator
+	httpClient            protoHttp.HttpClient
+	metrics               Metrics
+	metricMutex           sync.Mutex
+}
+
+type Metrics struct {
+	LastUseTS    int64
+	ResponseTime int64
 }
 
 var ErrNoSuitableEndpoint = errors.New("no suitable endpoint found")
@@ -21,7 +32,7 @@ func NewCommunicatorFromEndpoint(ctx context.Context, endpoint *protoCommon.Endp
 	componentCommunicator, err := communication.NewComponentCommunicatorFromEndpoint(endpoint)
 	if err == nil {
 		communicator := &Communicator{
-			ComponentCommunicator: *componentCommunicator,
+			componentCommunicator: *componentCommunicator,
 		}
 		if err = communicator.Ping(ctx); err == nil {
 			communicator.httpClient = protoHttp.NewHttpClient(componentCommunicator.GrpcClient)
@@ -31,19 +42,59 @@ func NewCommunicatorFromEndpoint(ctx context.Context, endpoint *protoCommon.Endp
 	return nil, err
 }
 
+// Return best communicator for endpoints based on response time
+// If no valid communicator is found nil and an error is returned
 func NewCommunicator(ctx context.Context, endpoints []*protoCommon.Endpoint) (*Communicator, error) {
-	// choose the first reachable endpoint from the list
+	comms := make([]*Communicator, 0, len(endpoints))
 	for _, endpoint := range endpoints {
 		communicator, err := NewCommunicatorFromEndpoint(ctx, endpoint)
-		if err == nil {
-			return communicator, nil
+		if err != nil {
+			logger.Warnw("could not create communicator", "endpoint", endpoint, "error", err)
+		} else {
+			comms = append(comms, communicator)
 		}
-		logger.Warnw("could not create communicator", "endpoint", endpoint, "error", err)
 	}
-	return nil, ErrNoSuitableEndpoint
+
+	if len(comms) < 1 {
+		return nil, ErrNoSuitableEndpoint
+	}
+
+	sort.Slice(comms, func(i, j int) bool {
+		// could use more than just response time to make decision
+		return comms[i].metrics.ResponseTime < comms[j].metrics.ResponseTime
+	})
+	return comms[0], nil
+}
+
+func (communicator *Communicator) setLastUseTS(lastUseTS int64) {
+	communicator.metricMutex.Lock()
+	defer communicator.metricMutex.Unlock()
+	communicator.metrics.LastUseTS = lastUseTS
+}
+
+func (communicator *Communicator) setResponseTime(responseTime int64) {
+	communicator.metricMutex.Lock()
+	defer communicator.metricMutex.Unlock()
+	communicator.metrics.ResponseTime = responseTime
+}
+
+func (communicator *Communicator) GetMetrics() Metrics {
+	communicator.metricMutex.Lock()
+	defer communicator.metricMutex.Unlock()
+	return communicator.metrics
+}
+
+func (communicator *Communicator) Ping(ctx context.Context) error {
+	start := time.Now()
+	err := communicator.componentCommunicator.Ping(ctx)
+	communicator.setResponseTime(time.Since(start).Milliseconds())
+	return err
 }
 
 func (communicator *Communicator) ProcessRequest(ctx context.Context, request *Request) (*Response, error) {
+	requestTS := time.Now()
+	communicator.setLastUseTS(requestTS.Unix())
+
 	grpcStream, err := communicator.Stream(ctx)
 	if err != nil {
 		return nil, err
