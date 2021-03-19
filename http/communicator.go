29f2989fe04e3594afff6f8b75c369ef294b305d
themmj <themmj@users.noreply.github.com>
2021-03-19 14:13:29 +0100

    KU-112 Make metrics toggle-able and provide convenience methods

--- http/communicator.go
+++ http/communicator.go
@@ -19,7 +19,7 @@ var _ communication.RemoteComponent = &Communicator{}
 type Communicator struct {
 	grpcClient  grpc.ClientConnInterface
 	httpClient  protoHttp.HttpClient
-	metrics     Metrics
+	metrics     *Metrics
 	metricMutex sync.Mutex
 }
 
@@ -30,7 +30,7 @@ type Metrics struct {
 
 var ErrNoSuitableEndpoint = errors.New("no suitable endpoint found")
 
-func NewCommunicatorFromEndpoint(ctx context.Context, endpoint *protoCommon.Endpoint) (*Communicator, error) {
+func NewCommunicatorFromEndpoint(ctx context.Context, endpoint *protoCommon.Endpoint, withMetrics bool) (*Communicator, error) {
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%v", endpoint.Host, endpoint.Port), grpc.WithInsecure())
 	if err != nil {
 		return nil, fmt.Errorf("could not create connection to component: %w", err)
@@ -38,53 +38,71 @@ func NewCommunicatorFromEndpoint(ctx context.Context, endpoint *protoCommon.Endp
 	communicator := &Communicator{
 		grpcClient: conn,
 		httpClient: protoHttp.NewHttpClient(conn),
+		metrics:    nil,
 	}
-	if err = communicator.Ping(ctx); err == nil {
-		return communicator, nil
+	if withMetrics {
+		communicator.metrics = &Metrics{}
+		err = communicator.Ping(ctx)
 	}
 	return communicator, err
+}
+
+// create valid communicators for endpoints
+// if withMetrics is set, the slice will be sorted by metrics, so the first communicator is the best and the worst is last
+// otherwise the communicators will be in the same order as the endpoints
+func NewCommunicators(ctx context.Context, endpoints []*protoCommon.Endpoint, withMetrics bool) []*Communicator {
+	comms := make([]*Communicator, 0, len(endpoints))
+	for _, endpoint := range endpoints {
+		communicator, err := NewCommunicatorFromEndpoint(ctx, endpoint, withMetrics)
+		if err != nil {
+			logger.Warnw("could not create communicator", "endpoint", endpoint, "error", err.Error())
+		} else {
+			comms = append(comms, communicator)
+		}
+	}
+	if withMetrics {
+		sort.Slice(comms, func(i, j int) bool {
+			// could use more than just response time to make decision
+			return comms[i].metrics.ResponseTime < comms[j].metrics.ResponseTime
+		})
+	}
+	return comms
 }
 
 // Return best communicator for endpoints based on response time
 // If no valid communicator is found nil and an error is returned
 func NewCommunicator(ctx context.Context, endpoints []*protoCommon.Endpoint) (*Communicator, error) {
-	comms := make([]*Communicator, 0, len(endpoints))
-	for _, endpoint := range endpoints {
-		communicator, err := NewCommunicatorFromEndpoint(ctx, endpoint)
-		if err != nil {
-			logger.Warnw("could not create communicator", "endpoint", endpoint, "error", err)
-		} else {
-			comms = append(comms, communicator)
-		}
-	}
+	comms := NewCommunicators(ctx, endpoints, true)
 
 	if len(comms) < 1 {
 		return nil, ErrNoSuitableEndpoint
 	}
-
-	sort.Slice(comms, func(i, j int) bool {
-		// could use more than just response time to make decision
-		return comms[i].metrics.ResponseTime < comms[j].metrics.ResponseTime
-	})
 	return comms[0], nil
 }
 
 func (communicator *Communicator) setLastUseTS(lastUseTS int64) {
-	communicator.metricMutex.Lock()
-	defer communicator.metricMutex.Unlock()
-	communicator.metrics.LastUseTS = lastUseTS
+	if communicator.metrics != nil {
+		communicator.metricMutex.Lock()
+		defer communicator.metricMutex.Unlock()
+		communicator.metrics.LastUseTS = lastUseTS
+	}
 }
 
 func (communicator *Communicator) setResponseTime(responseTime int64) {
-	communicator.metricMutex.Lock()
-	defer communicator.metricMutex.Unlock()
-	communicator.metrics.ResponseTime = responseTime
+	if communicator.metrics != nil {
+		communicator.metricMutex.Lock()
+		defer communicator.metricMutex.Unlock()
+		communicator.metrics.ResponseTime = responseTime
+	}
 }
 
-func (communicator *Communicator) GetMetrics() Metrics {
-	communicator.metricMutex.Lock()
-	defer communicator.metricMutex.Unlock()
-	return communicator.metrics
+func (communicator *Communicator) GetMetrics() *Metrics {
+	if communicator.metrics != nil {
+		communicator.metricMutex.Lock()
+		defer communicator.metricMutex.Unlock()
+		return communicator.metrics
+	}
+	return &Metrics{}
 }
 
 func (communicator *Communicator) Ping(ctx context.Context) error {
@@ -118,4 +136,27 @@ func (communicator *Communicator) ProcessRequest(ctx context.Context, request *R
 
 func (communicator *Communicator) Stream(ctx context.Context) (protoHttp.Http_ProcessRequestClient, error) {
 	return communicator.httpClient.ProcessRequest(ctx)
+}
+
+func processRequestInternal(ctx context.Context, comms []*Communicator, request *Request) (*Response, error) {
+	for _, comm := range comms {
+		resp, err := comm.ProcessRequest(ctx, request)
+		if err == nil {
+			return resp, nil
+		}
+		logger.Warnw("communicator could not process request", "error", err.Error())
+	}
+	return nil, ErrNoSuitableEndpoint
+}
+
+// pretty quick but does not optimize load to endpoints based on metrics
+func ProcessRequest(ctx context.Context, endpoints []*protoCommon.Endpoint, request *Request) (*Response, error) {
+	comms := NewCommunicators(ctx, endpoints, false)
+	return processRequestInternal(ctx, comms, request)
+}
+
+// caution this causes n + 1 round trips with n being the number of endpoints
+func ProcessRequestMetricBased(ctx context.Context, endpoints []*protoCommon.Endpoint, request *Request) (*Response, error) {
+	comms := NewCommunicators(ctx, endpoints, true)
+	return processRequestInternal(ctx, comms, request)
 }
